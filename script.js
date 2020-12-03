90bdd4f24f4566998225fec8cb3bc641b22e00f0
Peter An <anpeteran2@gmail.com>
2020-12-03 23:44:04 +1300

    added logging messages

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const messages = {
     riddleMessage: [
         'What has to be broken before you can use it?',
         'What kind of band never plays music?',
-        'What has words, but never speaks',
+        'What has words, but never speaks?',
         'I am an odd number. Take away a letter and I become even. What number am I?',
         'What five-letter word becomes shorter when you add two letters to it?',
         'I can be cracked, I can be made. I can be told, I can be played. What am I?'
@@ -38,3 +38,17 @@ const messages = {
     ]
 }
 
+const randomizeWord = array => {
+    const wordIndex = Math.floor(Math.random() * array.length)
+    return array[wordIndex];
+}
+
+const greetings = randomizeWord(messages[greetingsMessage]);
+const riddle = randomizeWord(messages[riddleMessage]);
+const answer = randomizeWord(messages[answerMessage]);
+const laugh = randomizeWord(messages[laughingMessage]);
+
+console.log('Maid Bot says: ', greetings);
+console.log('Let me tell you a riddle...', riddle);
+console.log('The answer was...', answer);
+console.log(laugh);
