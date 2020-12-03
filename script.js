bea5019f558a6c40639f30bb640d035893af5b6f
Peter An <anpeteran2@gmail.com>
2020-12-03 15:38:45 +1300

    added message object

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const 
+// theme: Riddle
+
+const messages = {
+    greetingsMessage: [
+        'Whats up',
+        'How are you today',
+        'Your Dumb',
+        '안녕하세요',
+        'こんにちは'
+    ],
+    riddleMessage: [
+        'What has to be broken before you can use it?',
+        'What kind of band never plays music?',
+        'What has words, but never speaks',
+        'I am an odd number. Take away a letter and I become even. What number am I?',
+        'What five-letter word becomes shorter when you add two letters to it?',
+        'I can be cracked, I can be made. I can be told, I can be played. What am I?'
+    ],
+    answerMessage: [
+        'An Egg',
+        'A rubber band',
+        'A book',
+        'Seven',
+        'Short',
+        'A Joke'
+    ],
+    laughingMessage: [
+        'HAHAHAHAHAHAHAHAH',
+        'LOOOOOOOOOOOOOOOL',
+        '...',
+        'Funny. Right?',
+        'WTF',
+        "What? That wasn't that boring",
+        'lul',
+        'KEKW',
+        'PEPEGA',
+        'Surely that one was good'
+    ]
+}
+
